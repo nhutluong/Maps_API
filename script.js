568f6f87d9bc48044bcc6939f6c6161280c09c9c
nhutluong <Nhut.Luong@mscoding.org>
2022-04-19 14:10:31 -0500

    created polygon based on center

--- script.js
+++ script.js
@@ -16,5 +16,23 @@ function LoadMap() {
         minZoom: 5,
         showSearchBar: true
     });
-
 }//end load map function
+
+function Polygon() {
+    //Loads the map
+    var myMap = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        center: new Microsoft.Maps.Location(30.392834, -88.887085),
+        zoom: 12
+    });
+    var center = map.getCenter();
+
+    var polygon = new Microsoft.Maps.Polygon([
+
+        new Microsoft.Maps.Location(center.latitude - 0.05, center.longitude - 0.05),
+        new Microsoft.Maps.Location(center.latitude + 0.01, center.longitude - 0.05),
+        new Microsoft.Maps.Location(center.latitude + 0.01, center.longitude + 0.05)], null);
+
+    map.entities.push(polygon);
+}//end draw polygon function
+
+
