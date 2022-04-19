e140ac09b8b941f15216510af9e554c817143d3f
nhutluong <Nhut.Luong@mscoding.org>
2022-04-19 13:57:12 -0500

    added zoom function

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // JavaScript source code
 
 var myMap   = undefined;
-var mapDiv  = undefined;
-function loadMapScenario() {
-    myMap = document.getElementById(('myMap'), {});
 
-    mapDiv = new Microsoft.Maps.Map(myMap);
-}//end function
+function LoadMap() {
+
+    //Loads the map
+    var myMap = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        center: new Microsoft.Maps.Location(30.392834, -88.887085),
+        zoom: 12
+    });
+
+    //Enables the search bar and sets up the zoom features
+    map.setOptions({
+        maxZoom: 15,
+        minZoom: 5,
+        showSearchBar: true
+    });
+
+}//end load map function
