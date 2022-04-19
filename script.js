1a6dba2b925a1cc6a9e689823ac0187a9bf32582
nhutluong <Nhut.Luong@mscoding.org>
2022-04-19 14:13:33 -0500

    added function to get directions

--- script.js
+++ script.js
@@ -35,4 +35,32 @@ function Polygon() {
     map.entities.push(polygon);
 }//end draw polygon function
 
+const directionsButton = document.querySelector('#get-directions');
+directionsButton.addEventListener('click', function () { GetDirections(); })
+
+function GetDirections() {
+    var myMap = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+
+        /* No need to set credentials if already passed in URL */
+        center: new Microsoft.Maps.Location(30.392834, -88.887085),
+        zoom: 12
+    });
+
+    Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
+        var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
+
+        // Set Route Mode to driving
+        directionsManager.setRequestOptions({ routeMode: Microsoft.Maps.Directions.RouteMode.driving });
+        var waypoint1 = new Microsoft.Maps.Directions.Waypoint({ address: 'Gulfport', location: new Microsoft.Maps.Location(30.369768, -88.887085) });
+        var waypoint2 = new Microsoft.Maps.Directions.Waypoint({ address: 'Biloxi', location: new Microsoft.Maps.Location(30.392834, -89.09154) });
+        directionsManager.addWaypoint(waypoint1);
+        directionsManager.addWaypoint(waypoint2);
+
+        // Set the element in which the itinerary will be rendered
+        directionsManager.setRenderOptions({ itineraryContainer: document.getElementById('printoutPanel') });
+        directionsManager.calculateDirections();
+    });
+
+}//end get directions function
+
 
